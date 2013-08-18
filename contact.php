293ac630e580b8589da2e5eb6d61f5a17cd86742
Richard Hancock <ygrichman@mail.com>
2013-08-18 15:30:13 +0100

    Added Favicon and Started Contact Page

---
+++ contact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Richard Hancock: Contact Me</title>
+
+	<meta charset="UTF-8">
+
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="shortcut icon" href="res/favicon.ico" />
+
+	<script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
+
+</head>
+<body>
+	<h1>Richard Hancock</h1>
+	<h2>Programmer Portfolio(Under Construction)</h2>
+	
+	<div id="MainContentWrapper">
+		<h3>Contact Me</h3>
+		<p>
+			CONTACT FORM HERE
+			
+		</p>
+	</div>
+
+	<span id="Footer">Copyright &copy; 2013 Richard Hancock | <a href="http://www.spacecrazyproductions.com">Space Crazy Productions</a></span>
+</body>
+</html>
